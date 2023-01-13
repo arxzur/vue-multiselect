a1eb0d46395f3b825c39881d299608921f04f387
arxzur <arxzur@proton.me>
2023-01-13 17:22:50 +0100

    WIP

--- src/components/MultiSelect.vue
+++ src/components/MultiSelect.vue
@@ -162,12 +162,8 @@ const vClickOutside = {
     </div>
     <div class="w-1/12 text-xs">
       <div class="flex justify-end content-center items-start pr-2 pt-2">
-        <svg v-if="isMenuActive" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-          <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 15.75l7.5-7.5 7.5 7.5" />
-        </svg>
-        <svg v-else xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-          <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
-        </svg>
+        <font-awesome-icon v-if="isMenuActive" :icon="['fa', 'angle-up']" />
+        <font-awesome-icon v-else :icon="['fa', 'angle-down']" />
       </div>
     </div>
   </div>
